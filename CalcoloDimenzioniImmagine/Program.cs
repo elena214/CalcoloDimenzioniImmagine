c66f6cf24fcd53f28f40253e3222a56ad65c5bae
elena214 <elebe2104@gmail.com>
2020-11-09 11:09:43 +0100

    Aggiungere i file di progetto.

---
+++ CalcoloDimenzioniImmagine/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalcoloDimenzioniImmagine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dimenzioneImmagine, risultato;
+            Console.WriteLine("Inserire l'altezza dell'immagine");
+            int altezzaImmagine = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserire la larghezza dell'immagine");
+            int larghezzaImmagine = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserire da quanti bit è composta l'immagine");
+            string bit =
+            switch()
+            {
+                case 
+            }
+            int calcolo = ((altezzaImmagine * larghezzaImmagine) * 3) / 1024;
+            Console.WriteLine($"L'immagine occupa {calcolo} KB");
+        }
+    }
+}
